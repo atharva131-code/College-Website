453b7b606706614082ade6dc923307af7647201f
Atharva Mishra <atharvamishra131@gmail.com>
2026-05-04 16:01:45 +0530

    Fix trust proxy for Render

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ import Message from './models/Message.js'
 dotenv.config()
 
 const app = express()
+app.set('trust proxy', 1)
 const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
